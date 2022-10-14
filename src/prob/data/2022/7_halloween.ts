797fc839f55703e324b95d95d229d5fade3d5940
bennyk0406 <kbenny0406@gmail.com>
2022-10-14 18:26:39 +0900

    2022halloween

--- src/prob/data/2022/7_halloween.ts
+++ src/prob/data/2022/7_halloween.ts
@@ -2,435 +2,435 @@ const itemData = {
     high: [
         {
             name: "투자분석가 심리학자",
-            isEquipItem: true,
-            probability: 3.5
+            probability: 3.5,
+            isEquipItem: true
         },
         {
             name: "최고경영자 판사",
-            isEquipItem: true,
-            probability: 3.5
+            probability: 3.5,
+            isEquipItem: true
         },
         {
             name: "손목시계 테두리",
-            isEquipItem: true,
-            probability: 3.5
+            probability: 3.5,
+            isEquipItem: true
         },
         {
             name: "클래식 카 테두리",
-            isEquipItem: true,
-            probability: 3.5
+            probability: 3.5,
+            isEquipItem: true
         },
         {
-            name: "맞춤 제작 넥타이",
-            isEquipItem: true,
-            probability: 3.5
+            name: "시계 보관함",
+            probability: 3.5,
+            isEquipItem: true
         },
         {
             name: "자동차 번호판",
-            isEquipItem: true,
-            probability: 3.5
+            probability: 3.5,
+            isEquipItem: true
         },
         {
             name: "상속인의 지갑",
-            isEquipItem: true,
-            probability: 2.5
+            probability: 2.5,
+            isEquipItem: true
         },
         {
             name: "쇼퍼홀릭 영매",
-            isEquipItem: true,
-            probability: 1.2
+            probability: 1.2,
+            isEquipItem: true
         },
         {
             name: "영앤리치 도둑",
-            isEquipItem: true,
-            probability: 1.2
+            probability: 1.2,
+            isEquipItem: true
         },
         {
             name: "지름신 테두리",
-            isEquipItem: true,
-            probability: 1.2
+            probability: 1.2,
+            isEquipItem: true
         },
         {
             name: "호화로운 액자 테두리",
-            isEquipItem: true,
-            probability: 1.2
+            probability: 1.2,
+            isEquipItem: true
         },
         {
             name: "부의 대물림",
-            isEquipItem: true,
-            probability: 1.2
+            probability: 1.2,
+            isEquipItem: true
         },
         {
             name: "아타셰 케이스",
-            isEquipItem: true,
-            probability: 1.2
+            probability: 1.2,
+            isEquipItem: true
         },
         {
             name: "체크무늬 패턴 지갑",
-            isEquipItem: true,
-            probability: 1
+            probability: 1,
+            isEquipItem: true
         },
         {
             name: "프랑켄슈타인 의사",
-            isEquipItem: true,
-            probability: 1.2
+            probability: 1.2,
+            isEquipItem: true
         },
         {
             name: "뱀파이어 성직자",
-            isEquipItem: true,
-            probability: 1.2
+            probability: 1.2,
+            isEquipItem: true
         },
         {
             name: "저주받은 밤의 마녀",
-            isEquipItem: true,
-            probability: 1.2
+            probability: 1.2,
+            isEquipItem: true
         },
         {
             name: "할로윈의 악몽 마술사",
-            isEquipItem: true,
-            probability: 1.2
+            probability: 1.2,
+            isEquipItem: true
         },
         {
             name: "할로윈 호박바구니",
-            isEquipItem: true,
-            probability: 1.2
+            probability: 1.2,
+            isEquipItem: true
         },
         {
             name: "잭 오 랜턴의 새장",
-            isEquipItem: true,
-            probability: 1.2
+            probability: 1.2,
+            isEquipItem: true
         },
         {
             name: "Trick or Treat!",
-            isEquipItem: true,
-            probability: 1.2
+            probability: 1.2,
+            isEquipItem: true
         },
         {
             name: "말라버린 장미 덩굴",
-            isEquipItem: true,
-            probability: 1.2
+            probability: 1.2,
+            isEquipItem: true
         },
         {
             name: "할로윈 밤의 지갑",
-            isEquipItem: true,
-            probability: 1
+            probability: 1,
+            isEquipItem: true
         },
         {
             name: "할로윈 테두리",
-            isEquipItem: true,
-            probability: 1.2
+            probability: 1.2,
+            isEquipItem: true
         },
         {
             name: "뱀파이어 테두리",
-            isEquipItem: true,
-            probability: 1.2
+            probability: 1.2,
+            isEquipItem: true
         },
         {
             name: "호박 유령의 숲",
-            isEquipItem: true,
-            probability: 1.2
+            probability: 1.2,
+            isEquipItem: true
         },
         {
             name: "신비한 합창",
-            isEquipItem: true,
-            probability: 1
+            probability: 1,
+            isEquipItem: true
         },
         {
             name: "마법의 염색약",
-            isEquipItem: false,
-            probability: 3.8
+            probability: 3.8,
+            isEquipItem: false
         },
         {
             name: "깜짝 엽서",
-            isEquipItem: false,
-            probability: 3.8
+            probability: 3.8,
+            isEquipItem: false
         },
         {
             name: "징벌의 엽서",
-            isEquipItem: false,
-            probability: 3.8
+            probability: 3.8,
+            isEquipItem: false
         },
         {
             name: "낡은 징벌의 엽서",
-            isEquipItem: false,
-            probability: 3.8
+            probability: 3.8,
+            isEquipItem: false
         },
         {
             name: "주사위 엽서",
-            isEquipItem: false,
-            probability: 3.8
+            probability: 3.8,
+            isEquipItem: false
         },
         {
             name: "3티어 카드",
-            isEquipItem: false,
-            probability: 3.8
+            probability: 3.8,
+            isEquipItem: false
         },
         {
             name: "4티어 카드",
-            isEquipItem: false,
-            probability: 3.8
+            probability: 3.8,
+            isEquipItem: false
         },
         {
             name: "할로윈 기념 엽서",
-            isEquipItem: false,
-            probability: 4.7
+            probability: 4.7,
+            isEquipItem: false
         },
         {
             name: "유령 엽서",
-            isEquipItem: false,
-            probability: 4.7
+            probability: 4.7,
+            isEquipItem: false
         },
         {
             name: "할로윈 염색약",
-            isEquipItem: false,
-            probability: 4.7
+            probability: 4.7,
+            isEquipItem: false
         },
         {
             name: "할로윈 묘비석",
-            isEquipItem: false,
-            probability: 4.7
+            probability: 4.7,
+            isEquipItem: false
         },
         {
             name: "할로윈 확성기",
-            isEquipItem: false,
-            probability: 4.7
+            probability: 4.7,
+            isEquipItem: false
         },
         {
             name: "마녀의 연마제",
-            isEquipItem: false,
-            probability: 3
+            probability: 3,
+            isEquipItem: false
         }
     ],
     low: [
         {
             name: "투자분석가 심리학자",
-            isEquipItem: true,
-            probability: 0.7
+            probability: 0.7,
+            isEquipItem: true
         },
         {
             name: "최고경영자 판사",
-            isEquipItem: true,
-            probability: 0.7
+            probability: 0.7,
+            isEquipItem: true
         },
         {
             name: "손목시계 테두리",
-            isEquipItem: true,
-            probability: 0.7
+            probability: 0.7,
+            isEquipItem: true
         },
         {
             name: "클래식 카 테두리",
-            isEquipItem: true,
-            probability: 0.7
+            probability: 0.7,
+            isEquipItem: true
         },
         {
-            name: "맞춤 제작 넥타이",
-            isEquipItem: true,
-            probability: 0.7
+            name: "시계 보관함",
+            probability: 0.7,
+            isEquipItem: true
         },
         {
             name: "자동차 번호판",
-            isEquipItem: true,
-            probability: 0.7
+            probability: 0.7,
+            isEquipItem: true
         },
         {
             name: "상속인의 지갑",
-            isEquipItem: true,
-            probability: 0.5
+            probability: 0.5,
+            isEquipItem: true
         },
         {
             name: "쇼퍼홀릭 영매",
-            isEquipItem: true,
-            probability: 0.2
+            probability: 0.2,
+            isEquipItem: true
         },
         {
             name: "영앤리치 도둑",
-            isEquipItem: true,
-            probability: 0.2
+            probability: 0.2,
+            isEquipItem: true
         },
         {
             name: "지름신 테두리",
-            isEquipItem: true,
-            probability: 0.2
+            probability: 0.2,
+            isEquipItem: true
         },
         {
             name: "호화로운 액자 테두리",
-            isEquipItem: true,
-            probability: 0.2
+            probability: 0.2,
+            isEquipItem: true
         },
         {
             name: "부의 대물림",
-            isEquipItem: true,
-            probability: 0.2
+            probability: 0.2,
+            isEquipItem: true
         },
         {
             name: "아타셰 케이스",
-            isEquipItem: true,
-            probability: 0.2
+            probability: 0.2,
+            isEquipItem: true
         },
         {
             name: "체크무늬 패턴 지갑",
-            isEquipItem: true,
-            probability: 0.15
+            probability: 0.15,
+            isEquipItem: true
         },
         {
             name: "프랑켄슈타인 의사",
-            isEquipItem: true,
-            probability: 0.2
+            probability: 0.2,
+            isEquipItem: true
         },
         {
             name: "뱀파이어 성직자",
-            isEquipItem: true,
-            probability: 0.2
+            probability: 0.2,
+            isEquipItem: true
         },
         {
             name: "저주받은 밤의 마녀",
-            isEquipItem: true,
-            probability: 0.2
+            probability: 0.2,
+            isEquipItem: true
         },
         {
             name: "할로윈의 악몽 마술사",
-            isEquipItem: true,
-            probability: 0.2
+            probability: 0.2,
+            isEquipItem: true
         },
         {
             name: "할로윈 호박바구니",
-            isEquipItem: true,
-            probability: 0.2
+            probability: 0.2,
+            isEquipItem: true
         },
         {
             name: "잭 오 랜턴의 새장",
-            isEquipItem: true,
-            probability: 0.2
+            probability: 0.2,
+            isEquipItem: true
         },
         {
             name: "Trick or Treat!",
-            isEquipItem: true,
-            probability: 0.2
+            probability: 0.2,
+            isEquipItem: true
         },
         {
             name: "말라버린 장미 덩굴",
-            isEquipItem: true,
-            probability: 0.2
+            probability: 0.2,
+            isEquipItem: true
         },
         {
             name: "할로윈 밤의 지갑",
-            isEquipItem: true,
-            probability: 0.15
+            probability: 0.15,
+            isEquipItem: true
         },
         {
             name: "할로윈 테두리",
-            isEquipItem: true,
-            probability: 0.2
+            probability: 0.2,
+            isEquipItem: true
         },
         {
             name: "뱀파이어 테두리",
-            isEquipItem: true,
-            probability: 0.2
+            probability: 0.2,
+            isEquipItem: true
         },
         {
             name: "호박 유령의 숲",
-            isEquipItem: true,
-            probability: 0.2
+            probability: 0.2,
+            isEquipItem: true
         },
         {
             name: "마법의 염색약",
-            isEquipItem: false,
-            probability: 4.3
+            probability: 4.3,
+            isEquipItem: false
         },
         {
             name: "신분증",
-            isEquipItem: false,
-            probability: 4.3
+            probability: 4.3,
+            isEquipItem: false
         },
         {
             name: "사망 광고판",
-            isEquipItem: false,
-            probability: 4.3
+            probability: 4.3,
+            isEquipItem: false
         },
         {
             name: "부고기사",
-            isEquipItem: false,
-            probability: 4.3
+            probability: 4.3,
+            isEquipItem: false
         },
         {
             name: "고대의 제작서",
-            isEquipItem: false,
-            probability: 4.3
+            probability: 4.3,
+            isEquipItem: false
         },
         {
             name: "마법의 연마제",
-            isEquipItem: false,
-            probability: 4.3
+            probability: 4.3,
+            isEquipItem: false
         },
         {
             name: "명장의 망치",
-            isEquipItem: false,
-            probability: 4.3
+            probability: 4.3,
+            isEquipItem: false
         },
         {
             name: "큐피드의 화살",
-            isEquipItem: false,
-            probability: 4.3
+            probability: 4.3,
+            isEquipItem: false
         },
         {
             name: "고급 엽서",
-            isEquipItem: false,
-            probability: 4.3
+            probability: 4.3,
+            isEquipItem: false
         },
         {
             name: "징벌의 엽서",
-            isEquipItem: false,
-            probability: 4.3
+            probability: 4.3,
+            isEquipItem: false
         },
         {
             name: "깜짝 엽서",
-            isEquipItem: false,
-            probability: 4.3
+            probability: 4.3,
+            isEquipItem: false
         },
         {
             name: "확성기",
-            isEquipItem: false,
-            probability: 4.3
+            probability: 4.3,
+            isEquipItem: false
         },
         {
             name: "2티어 카드",
-            isEquipItem: false,
-            probability: 4.3
+            probability: 4.3,
+            isEquipItem: false
         },
         {
             name: "3티어 카드",
-            isEquipItem: false,
-            probability: 4.3
+            probability: 4.3,
+            isEquipItem: false
         },
         {
             name: "할로윈 기념 엽서",
-            isEquipItem: false,
-            probability: 6.2
+            probability: 6.2,
+            isEquipItem: false
         },
         {
             name: "유령 엽서",
-            isEquipItem: false,
-            probability: 6.2
+            probability: 6.2,
+            isEquipItem: false
         },
         {
             name: "할로윈 염색약",
-            isEquipItem: false,
-            probability: 6.2
+            probability: 6.2,
+            isEquipItem: false
         },
         {
             name: "할로윈 묘비석",
-            isEquipItem: false,
-            probability: 6.2
+            probability: 6.2,
+            isEquipItem: false
         },
         {
             name: "할로윈 확성기",
-            isEquipItem: false,
-            probability: 6.1
+            probability: 6.1,
+            isEquipItem: false
         },
         {
             name: "마녀의 연마제",
-            isEquipItem: true,
-            probability: 0.5
+            probability: 0.5,
+            isEquipItem: true
         }
     ]
 };
