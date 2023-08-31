ca2f43a69f2e71ce52745b8e4f676308deb8c2ec
bennyk0406 <kbenny0406@gmail.com>
2023-08-31 21:10:14 +0900

    2023summer

---
+++ src/prob/data/2023/6_summer.ts
@@ -0,0 +1,463 @@
+const itemData = {
+    high: [
+        {
+            name: "해양 경찰",
+            isEquipItem: true,
+            probability: 3.5
+        },
+        {
+            name: "해양 생물 심리학자",
+            isEquipItem: true,
+            probability: 3.5
+        },
+        {
+            name: "돌고래 환영 테두리",
+            isEquipItem: true,
+            probability: 3.5
+        },
+        {
+            name: "바다 장식 테두리",
+            isEquipItem: true,
+            probability: 3.5
+        },
+        {
+            name: "물결 사이 환상",
+            isEquipItem: true,
+            probability: 3.5
+        },
+        {
+            name: "신비한 바다 장신구",
+            isEquipItem: true,
+            probability: 3.5
+        },
+        {
+            name: "불량 레이서 코기와 개굴",
+            isEquipItem: true,
+            probability: 2.9
+        },
+        {
+            name: "대양의 수호자 테러리스트",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "산호초 군락 테두리",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "바다 거품 테두리",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "돌고래의 쉼터",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "빛나는 파도",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "심해의 비밀 지갑",
+            isEquipItem: true,
+            probability: 0.8
+        },
+        {
+            name: "서핑 마니아 마피아",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "해변의 스파이",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "인어공주 마담",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "잠수부 도굴꾼",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "바캉스 소녀 마녀",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "개구리 지갑",
+            isEquipItem: true,
+            probability: 0.8
+        },
+        {
+            name: "찬란한 아쿠아마린",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "여름 테두리",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "오리 튜브 테두리",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "휴양지의 해커",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "라이프가드 간호사",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "블루스크린 테두리",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "코기코기 테두리",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "블루스크린 명패",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "코기코기 명패",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "코기코기 지갑",
+            isEquipItem: true,
+            probability: 0.8
+        },
+        {
+            name: "피서지에서 생긴 일",
+            isEquipItem: true,
+            probability: 1
+        },
+        {
+            name: "신비한 합창",
+            isEquipItem: true,
+            probability: 0.8
+        },
+        {
+            name: "마법의 염색약",
+            isEquipItem: false,
+            probability: 4
+        },
+        {
+            name: "깜짝 엽서",
+            isEquipItem: false,
+            probability: 4
+        },
+        {
+            name: "징벌의 엽서",
+            isEquipItem: false,
+            probability: 4
+        },
+        {
+            name: "주사위 엽서",
+            isEquipItem: false,
+            probability: 4
+        },
+        {
+            name: "3티어 카드",
+            isEquipItem: false,
+            probability: 4
+        },
+        {
+            name: "4티어 카드",
+            isEquipItem: false,
+            probability: 4
+        },
+        {
+            name: "해변의 엽서",
+            isEquipItem: false,
+            probability: 5.8
+        },
+        {
+            name: "대양의 보배",
+            isEquipItem: false,
+            probability: 5.8
+        },
+        {
+            name: "해변가 확성기",
+            isEquipItem: false,
+            probability: 5.8
+        },
+        {
+            name: "유리병 편지",
+            isEquipItem: false,
+            probability: 5.8
+        },
+        {
+            name: "바다색 염색약",
+            isEquipItem: false,
+            probability: 5.7
+        }
+    ],
+    low: [
+        {
+            name: "해양 경찰",
+            isEquipItem: true,
+            probability: 0.7
+        },
+        {
+            name: "해양 생물 심리학자",
+            isEquipItem: true,
+            probability: 0.7
+        },
+        {
+            name: "돌고래 환영 테두리",
+            isEquipItem: true,
+            probability: 0.7
+        },
+        {
+            name: "바다 장식 테두리",
+            isEquipItem: true,
+            probability: 0.7
+        },
+        {
+            name: "물결 사이 환상",
+            isEquipItem: true,
+            probability: 0.7
+        },
+        {
+            name: "신비한 바다 장신구",
+            isEquipItem: true,
+            probability: 0.7
+        },
+        {
+            name: "불량 레이서 코기와 개굴",
+            isEquipItem: true,
+            probability: 0.5
+        },
+        {
+            name: "대양의 수호자 테러리스트",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "산호초 군락 테두리",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "바다 거품 테두리",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "돌고래의 쉼터",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "빛나는 파도",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "심해의 비밀 지갑",
+            isEquipItem: true,
+            probability: 0.1
+        },
+        {
+            name: "서핑 마니아 마피아",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "해변의 스파이",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "인어공주 마담",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "잠수부 도굴꾼",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "바캉스 소녀 마녀",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "개구리 지갑",
+            isEquipItem: true,
+            probability: 0.1
+        },
+        {
+            name: "찬란한 아쿠아마린",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "여름 테두리",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "오리 튜브 테두리",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "휴양지의 해커",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "라이프가드 간호사",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "블루스크린 테두리",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "코기코기 테두리",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "블루스크린 명패",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "코기코기 명패",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "코기코기 지갑",
+            isEquipItem: true,
+            probability: 0.1
+        },
+        {
+            name: "피서지에서 생긴 일",
+            isEquipItem: true,
+            probability: 0.18
+        },
+        {
+            name: "마법의 염색약",
+            isEquipItem: false,
+            probability: 4.4
+        },
+        {
+            name: "신분증",
+            isEquipItem: false,
+            probability: 4.4
+        },
+        {
+            name: "사망광고판",
+            isEquipItem: false,
+            probability: 4.4
+        },
+        {
+            name: "부고 기사",
+            isEquipItem: false,
+            probability: 4.4
+        },
+        {
+            name: "고대의 제작서",
+            isEquipItem: false,
+            probability: 4.4
+        },
+        {
+            name: "마법의 연마제",
+            isEquipItem: false,
+            probability: 4.4
+        },
+        {
+            name: "명장의 망치",
+            isEquipItem: false,
+            probability: 4.4
+        },
+        {
+            name: "큐피드의 화살",
+            isEquipItem: false,
+            probability: 4.4
+        },
+        {
+            name: "고급 엽서",
+            isEquipItem: false,
+            probability: 4.4
+        },
+        {
+            name: "징벌의 엽서",
+            isEquipItem: false,
+            probability: 4.4
+        },
+        {
+            name: "깜짝 엽서",
+            isEquipItem: false,
+            probability: 4.4
+        },
+        {
+            name: "확성기",
+            isEquipItem: false,
+            probability: 4.4
+        },
+        {
+            name: "2티어 카드",
+            isEquipItem: false,
+            probability: 4.4
+        },
+        {
+            name: "3티어 카드",
+            isEquipItem: false,
+            probability: 4.4
+        },
+        {
+            name: "해변의 엽서",
+            isEquipItem: false,
+            probability: 6
+        },
+        {
+            name: "대양의 보배",
+            isEquipItem: false,
+            probability: 6
+        },
+        {
+            name: "해변가 확성기",
+            isEquipItem: false,
+            probability: 6
+        },
+        {
+            name: "유리병 편지",
+            isEquipItem: false,
+            probability: 6
+        },
+        {
+            name: "바다색 염색약",
+            isEquipItem: false,
+            probability: 5.8
+        }
+    ]
+};
+
+export default itemData;
